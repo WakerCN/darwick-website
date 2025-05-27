f0e8825445ef96158db4f69cc6d5dcaba0a1822e
waker <1209562577@qq.com>
2025-05-27 10:44:04 +0800

    feat: adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2025-05-23 13:27:43
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-05-23 14:33:44
- * @FilePath: \darwick-website\src\App.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { Header } from './layout/Header';
 
 function App() {
   return (
-    <div className="mainWebsite">
+    <>
       <Header />
       <div className='webMain'>main content</div>
       <footer className='webFooter'>foot</footer>
-    </div>
+    </>
   );
 }
 
