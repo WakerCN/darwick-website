63562d38ce1be9924bcbefe251a59522d50efdae
StarOne <1209562577@qq.com>
2025-06-04 13:52:21 +0800

    🐞 fix <page> switch route dont scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 /*
  * @Author       : 魏威
  * @Date         : 2025-05-27 10:45
- * @LastEditTime : 2025-06-04 10:49
+ * @LastEditTime : 2025-06-04 13:51
  * @LastEditors  : StarOne
  * @Description  :
  */
+import { useEffect, useRef } from 'react';
 import { DWFooter } from './layout/Footer/Footer';
 import { Header } from './layout/Header/Header';
 // import { Home } from './layout/Home/Home';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 
 function App() {
+  const scrollEef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    scrollEef.current?.scrollTo({ top: 0 });
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
-      <div className="webMain" id={"WebMain"}>
+      <div className="webMain" id={'WebMain'} ref={scrollEef}>
         <Outlet />
         <DWFooter />
       </div>
