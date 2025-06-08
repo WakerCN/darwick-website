e721964e66b63cb168629fc09d6029b9cc88b0b3
StarOne <1209562577@qq.com>
2025-06-08 08:29:33 +0800

    ✨ feat <lng> add about lng

--- src/views/service/Repair/index.tsx
+++ src/views/service/Repair/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author       : 魏威
  * @Date         : 2025-06-03 09:35
- * @LastEditTime : 2025-06-03 11:26
+ * @LastEditTime : 2025-06-08 08:23
  * @LastEditors  : StarOne
  * @Description  :
  */
@@ -10,8 +10,11 @@ import './index.scss';
 import { businessList } from '@/constant/bussiness';
 import { DWICon } from '@/components/DWIcon';
 import { Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 export const RepairPage: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="repair-page">
       {businessList.map((bItem, index) => (
@@ -21,38 +24,42 @@ export const RepairPage: React.FC = () => {
             style={{ marginTop: index === 0 ? 0 : 16 }}
           >
             <DWICon iconName={bItem.iconName} style={{ marginRight: 8 }} />
-            {bItem.type}
+            {t(`machine.${bItem.key}`)}
             {bItem.type === '涡轮增压器' && (
               <div>
                 <Typography.Text>
-                  ⚙️ ABBVTR0/1、VTR4/P.D.E、VTC4/P、TPS48-61、TPL61-90、A型系列;
+                  ⚙️ ABBVTR0/1、VTR4/P.D.E、VTC4/P、TPS48-61、TPL61-90、A
+                  {t(`base.type`)} {t(`base.series`)};
                 </Typography.Text>
                 <br />
                 <Typography.Text>
-                  ⚙️
-                  MAN系列NR12-NR29、NA34~NA70、TCA33~TCA88、TCR12~TCR22、MET18SRC-MET26SRC、MET33~MET83/SB,SC,SD,SE
+                  ⚙️ MAN {t(`base.series`)}
+                  NR12-NR29、NA34~NA70、TCA33~TCA88、TCR12~TCR22、MET18SRC-MET26SRC、MET33~MET83/SB,SC,SD,SE
                 </Typography.Text>
                 <br />
-                <Typography.Text>⚙️ NAPIER 和 MAK、KBB系列</Typography.Text>
+                <Typography.Text>
+                  ⚙️ NAPIER {t(`base.and`)} MAK、KBB {t(`base.series`)}
+                </Typography.Text>
               </div>
             )}
             {bItem.type === '调速器' && (
               <div>
                 <Typography.Text>
-                  ⚙️
-                  WOODWARD系列、SG、PSG、UG8、UG40、PGA12-PGA58、PGA200-PGA500、RHD系列、NZ系列、OBAMA
-                  EUROPA、1102、1115、2231系列
+                  ⚙️ WOODWARD {t(`base.series`)}
+                  、SG、PSG、UG8、UG40、PGA12-PGA58、PGA200-PGA500、RHD{' '}
+                  {t(`base.series`)}、NZ {t(`base.series`)}、OBAMA
+                  EUROPA、1102、1115、2231 {t(`base.series`)}
                 </Typography.Text>
               </div>
             )}
             {bItem.type === '燃油泵' && (
               <div>
-                <Typography.Text>⚙️ 所有类型</Typography.Text>
+                <Typography.Text>⚙️ {t(`base.allType`)}</Typography.Text>
               </div>
             )}
             {bItem.type === '液压系统' && (
               <div>
-                <Typography.Text>⚙️ 所有类型</Typography.Text>
+                <Typography.Text>⚙️ {t(`base.allType`)}</Typography.Text>
               </div>
             )}
           </Typography.Title>
