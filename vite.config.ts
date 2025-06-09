5306f006e244a89c766fc063b5cca188411e3948
StarOne <1209562577@qq.com>
2025-06-09 08:18:53 +0800

    🌈 perf <pic> compress pictures by plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
 /*
  * @Author       : 魏威
  * @Date         : 2025-05-27 10:45
- * @LastEditTime : 2025-06-03 22:53
+ * @LastEditTime : 2025-06-08 21:51
  * @LastEditors  : StarOne
  * @Description  :
  */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), visualizer()],
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [
+    react(),
+    visualizer(),
+    ViteImageOptimizer({
+      /* pass your config */
+      png: {
+        quality: 80
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 70
+      }
+    })
+  ],
   server: {
     port: 3000
   },
