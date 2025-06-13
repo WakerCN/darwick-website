080fed036954691d97da29c320ada6b12a6b8be5
StarOne <1209562577@qq.com>
2025-06-13 11:17:03 +0800

    ✨ feat optimize

--- src/layout/Home/Home.tsx
+++ src/layout/Home/Home.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author       : 魏威
  * @Date         : 2025-05-27 13:48
- * @LastEditTime : 2025-06-13 10:05
+ * @LastEditTime : 2025-06-13 10:55
  * @LastEditors  : StarOne
  * @Description  :
  */
@@ -15,9 +15,19 @@ import { useTranslation } from 'react-i18next';
 import { ReactTyped } from 'react-typed';
 import BrandWall from './BrandWall';
 import './index.scss';
+import { DWICon } from '@/components/DWIcon';
+import { useNavigate } from 'react-router';
 
 export const Home: React.FC = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const navigate = useNavigate();
+
+  const isEn = i18n.language === 'en';
+
+  const handleJump = () => {
+    navigate('/service/repair');
+  };
 
   return (
     <div className="home-section">
@@ -25,7 +35,11 @@ export const Home: React.FC = () => {
         <Flex className="mask" vertical align="center" justify="center">
           <ReactTyped
             className="web-title"
-            strings={['WE CARE ABOUT OUR PROMISIE', 'DRIVE TO THE EXCELLENCE']}
+            strings={
+              isEn
+                ? ['WE CARE ABOUT OUR PROMISIE', 'DRIVE TO THE EXCELLENCE']
+                : ['我们信守承诺，追求卓越']
+            }
             typeSpeed={80}
             backDelay={3000}
             backSpeed={0}
@@ -34,17 +48,80 @@ export const Home: React.FC = () => {
           />
           <div className="web-desc">{t('home.desc')}</div>
         </Flex>
-
-        {/* <div className="description">
-          <div className="desc-1">
-            WE CARE ABOUT OUR PROMISIE,
-            <br />
-            DRIVE TO THE EXCELLENCE
-          </div>
-          <div className="desc-2">{t('home.desc')}</div>
-        </div> */}
-        
       </section>
+
+      <DWSection title={t('home.t3.title')}>
+        <Row className="business-area" gutter={[10, 10]}>
+          {businessList.map((bItem) => {
+            let descElem = null;
+            switch (bItem.type) {
+              case '涡轮增压器':
+                descElem = (
+                  <div>
+                    🌀 ABB {t('home.t3.series')}： VTR、VTC、TPL、TPS
+                    <br />
+                    🌀 MAN {t('home.t3.series')}： NR、NA、TCA、TCR、MET
+                    <br />
+                    🌀 NAPIER、MAK、KBB {t('home.t3.series')}
+                    ...
+                  </div>
+                );
+                break;
+              case '调速器':
+                descElem = (
+                  <div>
+                    🎛️ WOODWARD {t('home.t3.series')}：UG、PGA
+                    <br />
+                    🎛️ ZEXEL {t('home.t3.series')}：RHD
+                    <br />
+                    🎛️ YANMAR {t('home.t3.series')}：NZ
+                    <br />
+                    🎛️ OBAMA EUROPA、SG、PSG、1102、1115、2231
+                  </div>
+                );
+                break;
+              case '燃油泵':
+                descElem = <div>⚙️ {t('home.t3.allType')}</div>;
+                break;
+              case '液压系统':
+                descElem = <div>⚙️ {t('home.t3.allType')}</div>;
+                break;
+              default:
+                break;
+            }
+            return (
+              <Col
+                className="business-card"
+                span={12}
+                xs={24}
+                sm={24}
+                md={24}
+                lg={24}
+                xl={12}
+                xxl={12}
+                key={bItem.type}
+                onClick={handleJump}
+              >
+                <div className="card-content">
+                  <Flex
+                    className="card-title"
+                    vertical
+                    justify="center"
+                    align="center"
+                  >
+                    <DWICon
+                      iconName={bItem.iconName}
+                      style={{ width: 40, height: 40, color: '#fff' }}
+                    />
+                    <span>{t(`machine.${bItem.key}`)}</span>
+                  </Flex>
+                  <div className="card-desc">{descElem}</div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </DWSection>
 
       <DWSection title={t('home.t1')}>
         <BrandWall brands={brandList} />
@@ -84,62 +161,6 @@ export const Home: React.FC = () => {
             </Typography.Text>
           </div>
         </div>
-      </DWSection>
-
-      <DWSection title={t('home.t3.title')}>
-        <Row className="business-area" gutter={[10, 10]}>
-          {businessList.map((bItem) => {
-            let descElem = null;
-            switch (bItem.type) {
-              case '涡轮增压器':
-                descElem = (
-                  <div>
-                    ⚙️ ABB VTR0/1、TPL、A{t('home.t3.type')}{' '}
-                    {t('home.t3.series')}
-                    <br />
-                    ⚙️ MAN {t('home.t3.series')} NR、NA、TCA、TCR、MET
-                    <br />
-                    ⚙️ NAPIER {t('home.t3.and')} MAK、KBB {t('home.t3.series')}
-                    ...
-                  </div>
-                );
-                break;
-              case '调速器':
-                descElem = (
-                  <div>
-                    ⚙️ WOODWARD、SG、PSG、UG、PGA、RHD、NZ、OBAMA EUROPA
-                  </div>
-                );
-                break;
-              case '燃油泵':
-                descElem = <div>⚙️ {t('home.t3.allType')}</div>;
-                break;
-              case '液压系统':
-                descElem = <div>⚙️ {t('home.t3.allType')}</div>;
-                break;
-              default:
-                break;
-            }
-            return (
-              <Col
-                className="business-card"
-                span={12}
-                xs={24}
-                sm={24}
-                md={24}
-                lg={24}
-                xl={12}
-                xxl={12}
-                key={bItem.type}
-              >
-                <div className="card-content">
-                  <div className="card-title">{t(`machine.${bItem.key}`)}</div>
-                  <div className="card-desc">{descElem}</div>
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
       </DWSection>
 
       <DWSection title={t('home.t4.title')}>
