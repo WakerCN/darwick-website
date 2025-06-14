4fe5786aa9b65774f76cecf2217226be2a28e296
StarOne <1209562577@qq.com>
2025-06-14 16:51:20 +0800

    🐞 fix compatible antd5 to react19

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author       : 魏威
  * @Date         : 2025-05-27 10:45
- * @LastEditTime : 2025-06-05 13:47
+ * @LastEditTime : 2025-06-14 16:47
  * @LastEditors  : StarOne
  * @Description  :
  */
@@ -12,6 +12,7 @@ import '@/assets/iconfont/iconfont.js';
 import { RouterProvider } from 'react-router';
 import { router } from './routers/index.tsx';
 import './locales/i18n.ts';
+import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />
